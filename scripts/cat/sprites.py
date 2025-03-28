4e21330ce99644d795086e40fd481c51b48b8bda
flamedash2358 <lucykooistra@icloud.com>
2025-03-28 09:04:39 -0700

    Update sprites.py
    wetland herbs

--- scripts/cat/sprites.py
+++ scripts/cat/sprites.py
@@ -661,6 +661,10 @@ class Sprites:
         for row, dry in enumerate(dryherbs_data):
             for col, dryherbs in enumerate(dry):
                 self.make_group("medcatherbs", (col, 3), f"acc_herbs{dryherbs}")
+        # wetlandherbs
+        for row, wetherbs in enumerate(wetlandherbs_data):
+            for col, wetlandherbs in enumerate(wetherbs):
+                self.make_group("medcatherbs", (col, 3), f"acc_herbs{wetlandherbs}")
         # wild
         for row, wilds in enumerate(wild_data):
             for col, wild in enumerate(wilds):
