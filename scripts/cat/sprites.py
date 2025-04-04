b629519779b56ddb63f6382ade8eeaddd5cb0337
flamedash2358 <lucykooistra@icloud.com>
2025-04-03 20:38:40 -0700

    Bugfixing

--- scripts/cat/sprites.py
+++ scripts/cat/sprites.py
@@ -601,7 +601,7 @@ class Sprites:
                 "WISTERIA",
                 "ROSE MALLOW",
                 "PICKLEWEED",
-                "GOLDEN CREEPING JENNY"
+                "GOLDEN CREEPING JENNY",
             ],
         ]
         dryherbs_data = [["DRY HERBS", "DRY CATMINT", "DRY NETTLES", "DRY LAURELS"]]
@@ -666,7 +666,7 @@ class Sprites:
         # dryherbs
         for row, dry in enumerate(dryherbs_data):
             for col, dryherbs in enumerate(dry):
-                self.make_group("medcatherbs", (col, 5), f"acc_herbs{dryherbs}")
+                self.make_group("medcatherbs", (col, 4), f"acc_herbs{dryherbs}")
         # wild
         for row, wilds in enumerate(wild_data):
             for col, wild in enumerate(wilds):
