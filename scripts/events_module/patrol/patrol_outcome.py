f4ff2bf10ad77a9440b27a66073a1f7320b492ac
flamedash2358 <lucykooistra@icloud.com>
2025-05-30 21:24:54 -0700

    Update patrol_outcome.py

--- scripts/events_module/patrol/patrol_outcome.py
+++ scripts/events_module/patrol/patrol_outcome.py
@@ -496,7 +496,7 @@ class PatrolOutcome:
                         results.append(
                         event_text_adjust(
                             Cat,
-                            i18n.t("cat.history.leader_lost_lives.many") % {"count": lives_lost},
+                            i18n.t("leader_lost_lives.many") % {"count": lives_lost},
                             main_cat=_cat,
                             )
                         )
@@ -504,7 +504,7 @@ class PatrolOutcome:
                         results.append(
                         event_text_adjust(
                             Cat,
-                            i18n.t("cat.history.leader_lost_lives.one"),
+                            i18n.t("leader_lost_lives.one"),
                             main_cat=_cat,
                             )
                         ) 
@@ -513,7 +513,7 @@ class PatrolOutcome:
                     results.append(
                         event_text_adjust(
                             Cat,
-                            i18n.t("cat.history.leader_lost_lives.one"),
+                            i18n.t("leader_lost_lives.one"),
                             main_cat=_cat,
                         )
                     )
